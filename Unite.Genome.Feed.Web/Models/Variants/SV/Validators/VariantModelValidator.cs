dc8d686c3b56ad2499e4e2881c216c511cdfc95a
vulasau <valiantsin.ulasau@gmail.com>
2023-01-13 16:08:02 +0100

    Update VariantModelValidator.cs

--- Unite.Genome.Feed.Web/Models/Variants/SV/Validators/VariantModelValidator.cs
+++ Unite.Genome.Feed.Web/Models/Variants/SV/Validators/VariantModelValidator.cs
@@ -14,10 +14,25 @@ public class VariantModelValidator : AbstractValidator<VariantModel>
             .Must(value => value > 0).WithMessage("Should be greater than 0");
 
         RuleFor(model => model.End)
+            .Must(value => value > 0).WithMessage("Should be greater than 0");
+
+        RuleFor(model => model.OtherChromosome)
+            .NotEmpty().WithMessage("Should not be empty");
+
+        RuleFor(model => model.OtherStart)
             .NotEmpty().WithMessage("Should not be empty")
+            .Must(value => value > 0).WithMessage("Should be greater than 0");
+
+        RuleFor(model => model.OtherEnd)
             .Must(value => value > 0).WithMessage("Should be greater than 0");
 
         RuleFor(model => model.Type)
             .NotEmpty().WithMessage("Should not be empty");
+
+        RuleFor(model => model.FlankingSequenceFrom)
+            .MaximumLength(200).WithMessage("Maximum length is 200");
+
+        RuleFor(model => model.FlankingSequenceTo)
+            .MaximumLength(200).WithMessage("Maximum length is 200");
     }
 }
