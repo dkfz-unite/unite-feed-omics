ffcd2969230f5eb7e5ed4fd41900d40d2fe52260
vulasau <valiantsin.ulasau@gmail.com>
2025-04-23 10:20:15 +0200

    Enhance error handling in VariantsAnnotationWorker by adding specific logging for each annotation handler failure.

--- Unite.Genome.Feed.Web/Workers/VariantsAnnotationWorker.cs
+++ Unite.Genome.Feed.Web/Workers/VariantsAnnotationWorker.cs
@@ -52,9 +52,39 @@ public class VariantsAnnotationWorker : BackgroundService
         {
             try
             {
-                _smsAnnotationHandler.Handle(_options.SmBucketSize);
-                _cnvsAnnotationHandler.Handle(_options.CnvBucketSize);
-                _svsAnnotationHandler.Handle(_options.SvBucketSize);
+                try
+                {
+                    _smsAnnotationHandler.Handle(_options.SmBucketSize);
+                }
+                catch
+                {
+                    _logger.LogError("SmsAnnotationHandler failed");
+                    throw;
+                }
+
+                try
+                {
+                    _cnvsAnnotationHandler.Handle(_options.CnvBucketSize);
+                }
+                catch
+                {
+                    _logger.LogError("CnvsAnnotationHandler failed");
+                    throw;
+                }
+
+                try
+                {
+                    _svsAnnotationHandler.Handle(_options.SvBucketSize);
+                }
+                catch
+                {
+                    _logger.LogError("SvsAnnotationHandler failed");
+                    throw;
+                }
+
+                // _smsAnnotationHandler.Handle(_options.SmBucketSize);
+                // _cnvsAnnotationHandler.Handle(_options.CnvBucketSize);
+                // _svsAnnotationHandler.Handle(_options.SvBucketSize);
             }
             catch (Exception exception)
             {
