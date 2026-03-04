eb6cc4e4fb3161a1df346c5bdaef541165c21051
SemionV <semion.varennik@gmail.com>
2026-03-04 13:49:22 +0300

    Implement CnvProfile IndexingCache

--- Unite.Omics.Indices/Services/CnvProfileIndexingCache.cs
+++ Unite.Omics.Indices/Services/CnvProfileIndexingCache.cs
@@ -1,16 +1,64 @@
 using Microsoft.EntityFrameworkCore;
 using Unite.Data.Context;
+using Unite.Data.Context.Repositories.Extensions.Queryable;
+using Unite.Data.Entities.Omics.Analysis.Dna.Cnv;
 using Unite.Data.Entities.Specimens;
 
 namespace Unite.Omics.Indices.Services;
 
 public class CnvProfileIndexingCache(IDbContextFactory<DomainDbContext> dbContextFactory) : IndexingCache(dbContextFactory)
 {
+    public IEnumerable<Profile> CnvProfiles { get; private set; }
     public IEnumerable<Specimen> Specimens { get; private set; }
     public IEnumerable<Data.Entities.Omics.Analysis.Sample> Samples { get; private set; }
 
     protected override void Load(int[] ids)
     {
-        throw new NotImplementedException();
+        LoadCnvProfiles(ids).Wait();
+        LoadSamples().Wait();
+    }
+
+    async Task LoadCnvProfiles(int[] ids)
+    {
+        await using var dbContext = await DbContextFactory.CreateDbContextAsync();
+
+        CnvProfiles = await dbContext.CnvProfiles
+            .AsNoTracking()
+            .Where(x => ids.Contains(x.Id))
+            .ToArrayAsync();
+    }
+
+    private async Task LoadSamples()
+    {
+        await using var dbContext = await DbContextFactory.CreateDbContextAsync();
+
+        var sampleIds = CnvProfiles.Select(p => p.SampleId).Distinct().ToArray();
+
+        Samples = await dbContext.OmicsSamples
+            .AsNoTracking()
+            .Where(s => sampleIds.Contains(s.Id))
+            .ToListAsync();
+    }
+    
+    private async Task LoadSpecimens()
+    {
+        await using var dbContext = await DbContextFactory.CreateDbContextAsync();
+
+        var specimenIds = Samples
+            .Select(sample => sample.SpecimenId)
+            .Distinct()
+            .ToArray();
+
+        Specimens = await dbContext.Set<Specimen>()
+            .AsNoTracking()
+            .IncludeMaterial()
+            .IncludeLine()
+            .IncludeOrganoid()
+            .IncludeXenograft()
+            .IncludeMolecularData()
+            .IncludeInterventions()
+            .IncludeDrugScreenings()
+            .Where(specimen => specimenIds.Contains(specimen.Id))
+            .ToArrayAsync();
     }
 }
