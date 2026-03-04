e87652ce22751d21fdd3d0850a2c89599431de68
SemionV <semion.varennik@gmail.com>
2026-03-04 14:24:00 +0300

    CnvProfile IndexEntityBuilder implementation

--- Unite.Omics.Indices/Services/CnvProfileIndexEntityBuilder.cs
+++ Unite.Omics.Indices/Services/CnvProfileIndexEntityBuilder.cs
@@ -1,3 +1,5 @@
+using Unite.Essentials.Extensions;
+using Unite.Indices.Entities.Basic.Specimens;
 using Unite.Indices.Entities.CnvProfiles;
 
 namespace Unite.Omics.Indices.Services;
@@ -6,6 +8,32 @@ public class CnvProfileIndexEntityBuilder: IndexEntityBuilder<CnvProfileIndex, C
 {
     public override CnvProfileIndex Create(int key, CnvProfileIndexingCache cache)
     {
-        throw new NotImplementedException();
+        var cnvProfile = cache.CnvProfiles.FirstOrDefault(x => x.Id == key);
+        if (cnvProfile == null)
+            return null;
+
+        var sample = cache.Samples.FirstOrDefault(x => x.Id == cnvProfile.SampleId);
+        if (sample == null)
+            return null;
+        
+        var specimen = cache.Specimens.FirstOrDefault(x => x.Id == sample.SpecimenId);
+        if(specimen == null)
+            return null;
+        
+        return new CnvProfileIndex
+        {
+            Chromosome = cnvProfile.Chromosome.ToString(),
+            ChromosomeArm = cnvProfile.ChromosomeArm.ToString(),
+            Gain =  cnvProfile.Gain,
+            Loss =  cnvProfile.Loss,
+            Neutral = cnvProfile.Neutral,
+            Id =  cnvProfile.Id,
+            Specimen = new SpecimenNavIndex
+            {
+                Id = specimen.Id,
+                ReferenceId =  specimen.ReferenceId,
+                Type = specimen.TypeId.ToDefinitionString()
+            }
+        };
     }
 }
